7d6915b7ec5508e46391ea3b5ba9cacf66d425ec
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-11-01 19:30:25 +0530

    feat: added dark mode animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
+import { StarBackground } from '../components/StarBackground';
+import { ThemeToggle } from '../components/ThemeToggle';
+
 export const Home = () => {
-  return <div>This is a home page</div>;
+  return (
+    <div className="min-h-screen">
+      This is a home page
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
+      {/* Navbar */}
+      {/* Main content */}
+      {/* Footer*/}
+    </div>
+  );
 };
