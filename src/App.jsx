0f280e33f61b3720b52518548c31d866622b14c9
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-11-01 19:30:08 +0530

    feat: added theme toggler

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Home } from './pages/Home';
 import { NotFound } from './pages/NotFound';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <h1>aibwgo</h1>
       <Routes>
         <Route index element={<Home />} />
         <Route path="*" element={<NotFound />} />
