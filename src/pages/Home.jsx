0802b58d893203ac39479281c1d43f4d311f24c8
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-11-06 14:36:31 +0530

    feat: added skills section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { AboutSection } from '../components/AboutSection';
 import { HeroSection } from '../components/HeroSection';
 import { Navbar } from '../components/Navbar';
+import { ProjectsSection } from '../components/ProjectsSection';
+import { SkillsSection } from '../components/SkillsSection';
 import { StarBackground } from '../components/StarBackground';
 import { ThemeToggle } from '../components/ThemeToggle';
 
@@ -17,6 +19,8 @@ export const Home = () => {
       <main>
         <HeroSection />
         <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
       </main>
       {/* Footer*/}
     </div>
